ce974af667bbac31bfb360fc36e172d8696e2dda
unknown <1036347723@qq.com>
2021-10-25 16:26:41 +0800

    实验一

--- MyTestOne/app/src/main/java/com/example/mytestone/MainActivity.java
+++ MyTestOne/app/src/main/java/com/example/mytestone/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.firstapp;
+package com.example.mytestone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.table_layout);
     }
-
-
 }
